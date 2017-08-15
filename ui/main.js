97575af88a5d058121ceabae8d94076ebc49f8ce
AppDev2227 <30623408+AppDev2227@users.noreply.github.com>
2017-08-15 23:37:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML='New Value';
 
 
 //Move the image
-alert(document.getElementById("madiOne").style.marginLeft);
+alert(document.getElementById('madi').style.marginLeft);
 var img = document.getElementById("madi");
 var marginLeft = 0;
 function moveRight()
