16fd5288c91091a55564e9d1b4beff0c6169d0e9
AppDev2227 <30623408+AppDev2227@users.noreply.github.com>
2017-08-15 23:44:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,13 +5,11 @@ element.innerHTML='New Value';
 
 
 //Move the image
-window.onload = function () {
-    var e = document.getElementById("madi");
-    e.innerHTML='Found you';
-};
+
 alert(document.getElementById('madi').style.marginLeft);
 var img = document.getElementById('madi');
 var marginLeft = 0;
+
 function moveRight()
 {
     marginLeft= marginLeft + 10;
