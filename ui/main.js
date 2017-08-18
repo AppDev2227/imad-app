e28dcec82e1cd6f309c54ce68faf081d009a79d4
AppDev2227 <30623408+AppDev2227@users.noreply.github.com>
2017-08-18 12:19:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,6 @@ element.innerHTML = ' New Value ONE ';
 var img = document.getElementById('madi');
 img.onclick = function() {
    
-    
+    var interval = setInterval(moveLeft,100);
   img.style.marginLeft = '200px';  
 };
