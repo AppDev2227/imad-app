65d34faa3222b91361066f90c1b825286dd9d8d7
AppDev2227 <30623408+AppDev2227@users.noreply.github.com>
2017-08-23 23:30:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,8 +151,8 @@ app.get('/articles/:articleName', function (req, res) {
     //articles[articleName]= {} content objecct for article one
    
    
-   // SELECT * FROM article WHERE title = 'article-one'
-   pool.query("SELECT * FROM article WHERE title = '"+ req.params.articleName + "'" , function(err,result){
+   // SELECT * FROM article WHERE title = ''; DELETE WHERE a= 'asdf'
+   pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName] , function(err,result){
        if(err){
            res.status(500).send(err.toString());
        } else {
